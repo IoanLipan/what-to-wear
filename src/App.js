46d2a572dae48a8e172671e79a063bb118e2a944
Lipan Ioan <lipanioan420@gmail.com>
2023-04-24 19:15:55 +0300

    config project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import SearchBar from './components/SearchBar';
+import TimeAndLocation from './components/TimeAndLocation';
+import TemperatureAndDetails from './components/TemperatureAndDetails';
+import Forecast from './components/Forecast';
+import getFormattedWeatherData from './services/weatherService';
 
 function App() {
+
+  const fetchWeather = async () => {
+    const data = await getFormattedWeatherData({units: "metric", q: "london" });
+    console.log(data);
+  }
+
+  fetchWeather();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen mx-auto w-full py-2 px-5 bg-gradient-to-br from-cyan-700 to-blue-700 shadow-xl shadow-gray-500">
+      <TimeAndLocation />
+      <SearchBar />
+      <TemperatureAndDetails />
+      <h1 className="text-2xl text-center font-bold text-white">What do I wear today?</h1>
+      <Forecast title="Hourly forecast" />
+      <Forecast title="Daily forecast" />
     </div>
   );
 }
